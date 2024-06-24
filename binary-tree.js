62f8d14614b75df87d92a3c2dff9760c74cb887a
Philippe Ducasse <ducassephi@hotmail.fr>
2024-06-24 19:25:01 +0200

    created find method for BST

--- binary-tree.js
+++ binary-tree.js
@@ -1,46 +1,62 @@
 class Node {
-    constructor(value){
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 class BinarySearchTree {
-    constructor(){
-        this.root = null;
-    }
-    insert(value){
-        let newNode = new Node(value);
-        if(!this.root){
-            this.root = newNode;
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value === current.value) return undefined;
+        if (value < current.value) {
+          if (!current.left) {
+            current.left = newNode;
             return this;
-        }else{
-            let current = this.root;
-            while(true){
-                if (value === current.value) return undefined;
-                if(value < current.value){
-                    if(!current.left){
-                        current.left = newNode;
-                        return this;
-                    } else {
-                        current = current.left;
-                    }
-                } else if (value > current.value){
-                    if(!current.right){
-                        current.right = newNode;
-                        return this;
-                    } else {
-                        current = current.right;
-                    }
-                } 
-            }
-            
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (!current.right) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
         }
+      }
     }
+  }
+  find(value) {
+    if (!this.root) return false;
+    let current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        console.log("FOUND: ", current.value);
+        return true;
+      }
+    }
+    console.log("Not found");
+    return false;
+  }
 }
 
 let tree = new BinarySearchTree();
 tree.insert(10);
 tree.insert(5);
 tree.insert(2);
-console.log(tree)
+tree.find(5);
