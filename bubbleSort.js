e210c9e342e5300248a195c449146d4936af4c3a
Philippe Ducasse <ducassephi@hotmail.fr>
2024-07-08 20:26:01 +0200

    completed bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,7 +1,9 @@
 const bubbleSort = (array) => {
     for (let i = 0; i< array.length; i++){
-        if(array[i] > array [i+1]){
-            [array[i], array[i + 1]] = [array[i+1], array[i]];
+        for (let j = 0; j < array.length-1-i; j++){
+            if(array[j] > array [j+1]){
+                [array[j], array[j+1]] = [array[j+1], array[j]];
+            }
         }
     } return array;
 }
