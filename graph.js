29e588da25b780ec9c4dab5a70d2d3756938598e
Philippe Ducasse <ducassephi@hotmail.fr>
2024-08-07 13:09:43 +0200

    added graph traversal methods

--- graph.js
+++ graph.js
@@ -1,37 +1,110 @@
-class Graph{
-    constructor(){
-        this.adjacencyList = {};
-    };
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
 
-    addVertex(vertex){
-        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
-    };
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
 
-    addEdge(vertex1, vertex2){
-        if(this.adjacencyList[vertex1]) this.adjacencyList[vertex1].push(vertex2);
-        if(this.adjacencyList[vertex2]) this.adjacencyList[vertex2].push(vertex1);
-    };
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1]) this.adjacencyList[vertex1].push(vertex2);
+    if (this.adjacencyList[vertex2]) this.adjacencyList[vertex2].push(vertex1);
+  }
 
-    removeEdge(vertex1, vertex2){
-        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(vertex => vertex !== vertex2);
-        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(vertex => vertex !== vertex1)
-    };
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (vertex) => vertex !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (vertex) => vertex !== vertex1
+    );
+  }
 
-    removeVertex(vertex){
-        for(let v of this.adjacencyList[vertex]){
-            this.removeEdge(v, vertex);
-        };
-        delete this.adjacencyList[vertex];
+  removeVertex(vertex) {
+    for (let v of this.adjacencyList[vertex]) {
+      this.removeEdge(v, vertex);
     }
+    delete this.adjacencyList[vertex];
+  }
+
+  depthFirstRecursive(vertex) {
+    const list = [];
+    const visited = {};
+
+    const DFS = (vertex) => {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      list.push(vertex);
+      this.adjacencyList[vertex].forEach((neighbour) => {
+        if (!visited[neighbour]) DFS(neighbour);
+      });
+    };
+    DFS(vertex);
+    console.log(list);
+    return list;
+  }
+
+  depthFirstIterative(vertex) {
+    const stack = [vertex];
+    const result = [];
+    const visited = {};
+
+    visited[vertex] = true;
+
+    while (stack.length) {
+      let popped = stack.pop();
+      result.push(popped);
+
+      this.adjacencyList[popped].forEach((v) => {
+        if (!visited[v]) {
+          visited[v] = true;
+          stack.push(v);
+        }
+      });
+    }
+    console.log(result);
+    return result;
+  }
+
+  bfs(vertex){
+    const queue = [vertex];
+    const result = [];
+    const visited = {};
+
+    visited[vertex] = true;
+
+    let currentVertex;
+    while(queue.length){
+        currentVertex = queue.shift();
+        result.push(currentVertex);
+
+        this.adjacencyList[currentVertex].forEach(v => {
+            if(!visited[v]){
+                visited[v] = true;
+                queue.push(v);
+            }
+        });
+    }
+    console.log(result);
+    return result;
+  }
 }
 
 g = new Graph();
-g.addVertex("SF");
-g.addVertex("CH");
-g.addVertex("ZI");
-g.addEdge("SF", "CH")
-g.addEdge("ZI", "SF");
-g.removeEdge("SF", "CH")
-g.removeVertex("ZI")
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
 
-console.log(g)
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+
+g.bfs("A");
